2ca60bd23ddd4ef6bfe13fd84026e2b32c8f2010
Robert Yuen <dungeonry@hotmail.com>
2018-12-17 13:58:41 -0800

    Added webpack bundle analyzer plugin and source-maps.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const {
+    BundleAnalyzerPlugin
+} = require("webpack-bundle-analyzer");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
@@ -35,7 +38,7 @@ module.exports = {
             template: path.join(__dirname, "src/templates/index.html"),
             filename: "index.html",
             inject: "body",
-            title: "Front End Boilerplate Project"
+            title: "Pdthunt Clone"
         }),
         new CopyWebpackPlugin([{
             from: "./src/images/*.ico",
@@ -47,6 +50,12 @@ module.exports = {
         }], {
             debug: "debug",
             copyUnmodified: true
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+            reportFilename: "bundle_sizes.html"
         })
-    ]
+    ],
+    devtool: "source-map"
 };
